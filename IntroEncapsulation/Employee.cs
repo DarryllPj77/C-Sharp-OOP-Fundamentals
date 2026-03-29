d89f36ab637f47523b8b687dc524a82ac8187f42
DarryllPj77 <pajaganasdarryll2004@gmail.com>
2026-03-29 10:29:20 +0800

    feat: add IntroEncapsulation project introducing encapsulation concepts

---
+++ IntroEncapsulation/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroEncapsulation
+{
+    internal class Employee
+    {
+        // Fields or Attributes of the Object -> 
+        private string name;
+        private int employeeId;
+        private double salary;
+
+        // Constructor
+        public Employee(string name, int employeeId, double salary)
+        {
+            this.name = name;
+            this.employeeId = employeeId;
+            this.salary = salary;
+        }
+
+        // Properties to access and modify private fields
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int EmployeeId
+        {
+            get { return employeeId; }
+        }
+
+        public double Salary
+        {
+            get { return salary; }
+            set
+            {
+                if (value >= 0)
+                {
+                    salary = value;
+                }
+                else
+                {
+                    Console.WriteLine("Salary cannot be negative.");
+                }
+            }
+        }
+
+        public void DisplayEmployeeInfo()
+        {
+            Console.WriteLine($"Employee ID: {EmployeeId}"); // ✅ Using property
+            Console.WriteLine($"Name: {Name}"); // ✅ Using property
+            Console.WriteLine($"Salary: ${salary}"); // ❌ Using field directly - Bad Practice; for practice purposes its fine
+        }
+    }
+}
---
+++ IntroEncapsulation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee("Darryll", 1, 5000);
+            employee1.DisplayEmployeeInfo();
+        }
+    }
+}

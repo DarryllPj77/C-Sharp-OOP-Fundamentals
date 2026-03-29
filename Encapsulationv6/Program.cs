fa96fd623864588b396fcd59087a9b22689966c9
DarryllPj77 <pajaganasdarryll2004@gmail.com>
2026-03-29 10:28:46 +0800

    feat: add Encapsulationv6 with method-based encapsulation approach

---
+++ Encapsulationv6/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace Encapsulationv6
+{
+    // STYLE 6 — Method-Based Encapsulation - No public properties.
+    internal class Program
+    {
+        class Person
+        {
+            private string name;
+            private int age;
+
+            public Person(string name, int age)
+            {
+                this.name = name;
+                this.age = age;
+            }
+
+            public void SetName(string newName)
+            {
+                if (!string.IsNullOrEmpty(newName))
+                {
+                    name = newName;
+                }
+            }
+
+            public void SetAge(int newAge)
+            {
+                if(newAge >= 0)
+                {
+                    age = newAge;
+                }
+            }
+
+            public string GetName()
+            {
+                return name;
+            }
+
+            public int GetAge()
+            {
+                return age;
+            }
+
+            public void ShowInfo()
+            {
+                Console.WriteLine("Name: " + name);
+                Console.WriteLine("Age: " + age);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introductory for OOP Fundamentals");
+            Console.WriteLine("----------ENCAPSULATIONv6----------");
+
+            Person person1 = new Person("Darryll", 21);
+
+            person1.ShowInfo();
+
+            person1.SetName("Celeine");
+            person1.SetAge(21);
+            person1.ShowInfo();
+        }
+    }
+}

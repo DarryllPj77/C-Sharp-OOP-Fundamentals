f5e6964d5225d06a43189cc047dd501610bd526c
DarryllPj77 <pajaganasdarryll2004@gmail.com>
2026-03-29 10:19:57 +0800

    feat: add IntroClass project with class fundamentals and instance members

---
+++ IntroClass/Classes/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroClass.Classes
+{
+    internal class Account
+    {
+        public int AccountNumber { get; set; }
+
+        public double Balance { get; private set; }
+    }
+}
---
+++ IntroClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using IntroClass.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroClass
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            // Create Bank
+            Bank bank = new Bank(); // -> instance of a back called Object
+
+            // Create Customer
+            Customer customer1 = new Customer();
+            customer1.Name = "Darryll";
+            customer1.Address = "Blk 22 Lot 1 St.Santol Palmera Woodlands";
+            bank.Customers.Add(customer1);
+        }
+    }
+}

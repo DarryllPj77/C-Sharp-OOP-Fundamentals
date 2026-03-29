af640c8ef9052a5e924107d08b3c30ed92b1f1d4
DarryllPj77 <pajaganasdarryll2004@gmail.com>
2026-03-29 10:25:42 +0800

    feat: add IntroConstructors project demonstrating constructor overloading

---
+++ IntroConstructors/Students.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroConstructors
+{
+    internal class Students
+    {
+        // Auto-implemented properties
+        public string name { get; set; } // default value null
+        public int Age { get; set; } // default value = 0
+        public double Grade { get; set; }  // default value = 0
+
+        // Default Constructors
+        // Parameterized Constructors
+        // Private Constructors
+        // Static Constructors
+        // Copy Constructors
+
+
+    }
+}

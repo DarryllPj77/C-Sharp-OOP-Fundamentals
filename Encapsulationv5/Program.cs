1f9cb85bb4e2c2ad6c6893e8e40090943d36ed8c
DarryllPj77 <pajaganasdarryll2004@gmail.com>
2026-03-29 10:28:31 +0800

    feat: add Encapsulationv5 showcasing encapsulation best practices

---
+++ Encapsulationv5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Encapsulationv5
+{
+    // STYLE 5 — Write-Only Property (Rare) - You can set but cannot read:
+    internal class Program
+    {
+        class Person
+        {
+            private string name;
+            private int age;
+
+            public string Name { set { name = value; } }
+            public int Age { set { age = value; } }
+
+            public Person(string name, int age)
+            {
+                this.name = name;
+                this.age = age;
+            }
+
+            public void ShowInfo()
+            {
+                Console.WriteLine("Name: " + name);
+                Console.WriteLine("Age: " + age);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introductory for OOP Fundamentals");
+            Console.WriteLine("----------ENCAPSULATIONv5----------");
+
+            Person person1 = new Person("Darryll", 21);
+
+            person1.ShowInfo();
+
+            person1.Name = "New";
+            person1.Age = 31;
+            person1.ShowInfo();
+
+            // string nameValue = person1.Name;  // ❌ ERROR - Cannot read write-only property
+            // int ageValue = person1.Age;       // ❌ ERROR - Cannot read write-only property
+        }
+    }
+}
---
+++ Encapsulationv5/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Encapsulationv5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Encapsulationv5")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("625451b4-ef6f-4d7e-a903-cdea3d8d9582")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
